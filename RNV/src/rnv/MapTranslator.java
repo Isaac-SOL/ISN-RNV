a6186c194638c6bd662a921808031b6ae4c3c7e1
Dironiil <berthomet.g@dbmail.com>
2016-04-05 00:04:18 +0200

    Fin du mapTranslator, code testé en console
    - Dans la méthode textToTable :
      * La variable lines s'appelle maintenant nbColumns (car elle compte le nombre de colonnes du tableau / de la map)
      * La variable columns s'appelle maintenant nbLines (car elle compte le nombre de lignes du tableau / de la map)
    
      * Lignes 41 et 42 : correction en mettant les bonnes valeurs dans le subString (-> subString(a,b) : la valeur à l'index a est comprise dans le subString mais PAS la valeur en l'index b)
    
      * Lignes 47 et 48 : simplification du code (changement "... <= x - 1" par "... < x" dans les if)
    
      * Ligne 49 : rajout de la fonction Character.getNumericValue (sinon le int retourné n'était pas la bonne valeur, sûrement la valeur ASCII du char) + correction de " (j*nbColumns)+1) " par " (j*nbColumns)+i) "
    
    - Dans la méthode tableToText : rajout de la partie ajoutant le contenu des tiles au string

--- RNV/src/rnv/MapTranslator.java
+++ RNV/src/rnv/MapTranslator.java
@@ -1,29 +1,53 @@
 /*
- * To change this template, choose Tools | Templates
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-package rnv;
+package test;
 
 /**
  *
- * @author ubuntu
+ * @author Guillaume
  */
-public class MapTranslator {
+public class Test {
 
-        //Transformation du String en Tableau
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String sTest = "0305012340123401234";
+        int columns = Integer.parseInt(sTest.substring(0,2));
+        int lines = Integer.parseInt(sTest.substring(2,4));
+        
+        System.out.println ("nb l : " + lines + ", nb c : " + columns);
+        System.out.println(sTest.substring(4));
+        
+        int[][] tTest;
+        tTest = textToTable(sTest);
+        
+        sTest = "fatboy";
+        
+        sTest = tableToText(tTest);
+        
+        System.out.println(sTest);
+        
+        
+    }
+    
+    
+    //Transformation du String en Tableau
     public static int[][] textToTable(String sMap) {
 
-       int lines = Integer.parseInt(sMap.substring(0,1));
-       int columns = Integer.parseInt(sMap.substring(2,3));
+       int nbColumns = Integer.parseInt(sMap.substring(0,2));
+       int nbLines = Integer.parseInt(sMap.substring(2,4));
        String tiles = sMap.substring(4);
 
-       int[][] tMap = new int[lines][columns];      //Création du tableau avec lignes et colonnes
+       int[][] tMap = new int[nbColumns][nbLines];      //Création du tableau avec lignes et colonnes
 
-        for(int j = 0; j <= columns - 1; j++) {
-            for(int i = 0; i <= lines - 1; i++) {
-                tMap[i][j] = tiles.charAt((j*lines)+1);        //Remplit le tableau avec les valeurs du string
-                System.out.println("Ligne " + lines + ", colonne " + columns + ": " + tMap[i][j]);
+        for(int j = 0; j < nbLines; j++) {
+            for(int i = 0; i < nbColumns; i++) {
+                tMap[i][j] = Character.getNumericValue(tiles.charAt((j*nbColumns)+i));        //Remplit le tableau avec les valeurs du string
+                System.out.println("Colonne " + (i+1) + ", ligne " + (j+1) + ": " + tMap[i][j]);
             }
         }
 
@@ -51,7 +75,18 @@ public class MapTranslator {
         }
 
         String sMap = sLines + sColumns;// </editor-fold>
+        
+        // <editor-fold defaultstate="collapsed" desc="Conversion du contenu des cases en String">
+        String sTilesContent = "";
+        for (int j = 0; j < tMap[0].length; j++) {
+            for (int i = 0; i < tMap.length; i++) {
+                sTilesContent += tMap[i][j] + "";
+            }
+        }
+        
+        sMap += sTilesContent; //</editor-fold>
 
         return sMap;
     }
+    
 }
