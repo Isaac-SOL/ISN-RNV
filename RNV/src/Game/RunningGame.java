55d6271ad39f0df5bee169397d0c746696568a69
Isaac-SOL <pi.pou.let@hotmail.fr>
2016-04-18 23:07:24 +0200

    getViewRadius devrait fonctionner
    + Rajouté getTokenXPos et getTokenYPos pour trouver les coords du Token (duh)

--- RNV/src/Game/RunningGame.java
+++ RNV/src/Game/RunningGame.java
@@ -22,7 +22,6 @@ public class RunningGame {
         
     }
     
-    //TODO Peut-être qu'il faudra caler tout ça dans un Tool? Le MapTranslator peut-être?
     public RunningGame(Integer[][] map) {
         currentMap = intToTiles(map);
     }
@@ -64,8 +63,47 @@ public class RunningGame {
         return tilesToInt(currentMap);
     }
     
-    public void getViewRadius(int r) {   //Contiendra une table de 5x5 qui représentera la "vue" du Token. Sera utilisée notamment par le RNV. Note: Je peux aussi faire dans l'overkill et mettre un radius variable. Je vais voir ça
-        Tile[][] viewRadius = new Tile[2*r + 1][2*r + 1];
+    //Choper la coordonnée X du Token. TODO Rajouter des exceptions, rendre ce commentaire propre (Dironiil)
+    public int getTokenXPos() {
+        int txpos = 0;
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                if ("Token".equals(currentMap[i][j].type)) {
+                    txpos = i;
+                }
+            }
+        }
+        return txpos;
+    }
+    
+    //Choper la coordonnée Y du Token. TODO Rajouter des exceptions, rendre ce commentaire propre (Dironiil)
+    public int getTokenYPos() {
+        int typos = 0;
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                if ("Token".equals(currentMap[i][j].type)) {
+                    typos = j;
+                }
+            }
+        }
+        return typos;
+    }
+    
+    //Contiendra une table qui représentera la "vue" du Token, dans un certain rayon. Sera utilisée notamment par le RNV. TODO Rendre ce commentaire propre (Dironiil)
+    public Tile[][] getViewRadius(int r) {
+        Tile[][] viewRadius = new Tile[2*r + 1][2*r + 1];       //Fixe la taille de la table de vision selon le rayon choisi
+        int XPos = getTokenXPos();
+        int YPos = getTokenYPos();
+        for (int i = -r; i <= r; i++) {
+            for (int j = -r; j <= r; j++) {
+                if (i >= 0 && i <= currentMap.length && j >= 0 && j <= currentMap[0].length) {      //Vérifie que les coordonnées soient bien dans la table
+                    viewRadius[i][j] = currentMap[XPos+i][YPos+j];      //Si elles sont dedans, copie la Tile aux coordonées correspondantes
+                } else {
+                    viewRadius[i][j] = new Tile("Obstacle");        //Sinon, remplace le vide par un obstacle
+                }
+            }
+        }
+        return viewRadius;
     }
     
     //Variables
